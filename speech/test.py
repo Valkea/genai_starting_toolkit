4d4afe21ee1f206bcf200888fd1a2a604aeda244
Emmanuel LETREMBLE <emmanuel.letremble@devoteam.com>
2024-06-27 16:10:57 +0200

    feature: first version of the Diarization system

---
+++ speech/test.py
@@ -0,0 +1,101 @@
+import os
+import time
+import azure.cognitiveservices.speech as speechsdk
+
+from dotenv import load_dotenv, dotenv_values 
+# loading variables from .env file
+load_dotenv() 
+ 
+
+def conversation_transcriber_recognition_canceled_cb(evt: speechsdk.SessionEventArgs):
+    print('Canceled event')
+
+def conversation_transcriber_session_stopped_cb(evt: speechsdk.SessionEventArgs):
+    print('SessionStopped event')
+
+def conversation_transcriber_transcribed_cb(evt: speechsdk.SpeechRecognitionEventArgs):
+    print('TRANSCRIBED:')
+    if evt.result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        print('\tText={}'.format(evt.result.text))
+        print('\tSpeaker ID={}'.format(evt.result.speaker_id))
+    elif evt.result.reason == speechsdk.ResultReason.NoMatch:
+        print('\tNOMATCH: Speech could not be TRANSCRIBED: {}'.format(evt.result.no_match_details))
+
+def conversation_transcriber_session_started_cb(evt: speechsdk.SessionEventArgs):
+    print('SessionStarted event')
+
+class BinaryFileReaderCallback(speechsdk.audio.PullAudioInputStreamCallback):
+    def __init__(self, filename: str):
+        super().__init__()
+        self._file_h = open(filename, "rb")
+
+    def read(self, buffer: memoryview) -> int:
+        try:
+            size = buffer.nbytes
+            frames = self._file_h.read(size)
+
+            buffer[:len(frames)] = frames
+
+            return len(frames)
+        except Exception as ex:
+            print('Exception in `read`: {}'.format(ex))
+            raise
+
+    def close(self) -> None:
+        print('closing file')
+        try:
+            self._file_h.close()
+        except Exception as ex:
+            print('Exception in `close`: {}'.format(ex))
+            raise
+
+def recognize_from_file():
+    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
+    speech_config.speech_recognition_language=os.environ.get('SPEECH_LANGUAGE')
+
+    filename = "test.mp3"
+    print("==>", filename[-3:]) 
+    if filename[-3:] == "wav":
+        # WAV
+        audio_config = speechsdk.audio.AudioConfig(filename=filename)
+    else:
+        # Creates an audio stream format. MP3 ...
+        compressed_format = speechsdk.audio.AudioStreamFormat(compressed_stream_format=speechsdk.AudioStreamContainerFormat.MP3)
+        callback = BinaryFileReaderCallback(filename=filename)
+        stream = speechsdk.audio.PullAudioInputStream(stream_format=compressed_format, pull_stream_callback=callback)
+        audio_config = speechsdk.audio.AudioConfig(stream=stream)
+
+    conversation_transcriber = speechsdk.transcription.ConversationTranscriber(speech_config=speech_config, audio_config=audio_config)
+
+    transcribing_stop = False
+
+    def stop_cb(evt: speechsdk.SessionEventArgs):
+        #"""callback that signals to stop continuous recognition upon receiving an event `evt`"""
+        print('CLOSING on {}'.format(evt))
+        nonlocal transcribing_stop
+        transcribing_stop = True
+
+    # Connect callbacks to the events fired by the conversation transcriber
+    conversation_transcriber.transcribed.connect(conversation_transcriber_transcribed_cb)
+    conversation_transcriber.session_started.connect(conversation_transcriber_session_started_cb)
+    conversation_transcriber.session_stopped.connect(conversation_transcriber_session_stopped_cb)
+    conversation_transcriber.canceled.connect(conversation_transcriber_recognition_canceled_cb)
+    # stop transcribing on either session stopped or canceled events
+    conversation_transcriber.session_stopped.connect(stop_cb)
+    conversation_transcriber.canceled.connect(stop_cb)
+
+    conversation_transcriber.start_transcribing_async()
+
+    # Waits for completion.
+    while not transcribing_stop:
+        time.sleep(.5)
+
+    conversation_transcriber.stop_transcribing_async()
+
+# Main
+
+try:
+    recognize_from_file()
+except Exception as err:
+    print("Encountered exception. {}".format(err))
